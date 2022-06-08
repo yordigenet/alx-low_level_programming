6182272cdb3358e4938b9304b5c04c6eaba60243
Yordanos_Girma <yordigenet@gmail.com>
2022-06-08 12:04:31 -0700

    assert

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,1 +1,17 @@
-assert positive_or_negative(0) == is zero
+#include "main.h"
+
+/**
+* main - tests function that prints if integer is positive or negative
+* Return: 0
+*/
+
+int main(void)
+{
+        int i;
+
+        i = 98;
+        positive_or_negative(i);
+	assert positive_or_negative(0) == is zero
+
+        return (0);
+}
