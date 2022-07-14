ede4686fa8bdeee380204fd1e943eadf6b4a61f8
Yordanos_Girma <yordigenet@gmail.com>
2022-07-14 07:46:21 -0700

    C - Singly linked lists

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -3,7 +3,7 @@
 
 /**
  * print_list - a function that prints all the elements of a linked list
- * h: pointer to the list_t list to print
+ * h: pointer to the list_t list to print.
  * Return: the number of nodes printed
  */
 size_t print_list(const list_t *h)
