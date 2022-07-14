80fe44188aa1f23d88279d64ce6593e3d9c3da36
Yordanos_Girma <yordigenet@gmail.com>
2022-07-14 08:22:18 -0700

    C - Singly linked lists

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -5,9 +5,9 @@
  * preceding: a function that prints a dialog
  * Return: 0 for succeses excution, non-zero otherwise
  */
-void __attribute__((constructor)) preceding();
+void preceding(void) __attribute__((constructor));
 
-void preceding();
+void preceding(void);
 {
 	printf("You're beat! and yet, you must allow,\nI bore my house upon my back!\n");
 	return(0);
